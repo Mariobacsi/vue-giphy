22f8f62b28e8962ab03b37a9f5b0a7337fb3219c
Mario Seidl <mario.s-@live.de>
2020-09-15 15:57:40 +0200

    Components definiert

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <searchbar/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+
+import Searchbar from "@/components/Searchbar";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Searchbar
   }
 }
 </script>
