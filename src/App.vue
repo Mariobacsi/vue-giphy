347faa1f2cd1379032a576d4b9264d40e7895b5b
Mario Seidl <mario.s-@live.de>
2020-09-15 16:09:08 +0200

    Fetchdata Method

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,40 @@
 <template>
-    <div id="app">
+  <div id="app">
 
-        <searchbar/>
+    <searchbar/>
 
-    </div>
+  </div>
 </template>
 
 <script>
 
 
-    import Searchbar from "@/components/Searchbar";
-    export default {
-        name: 'App',
-        components: {
-            Searchbar
-        }
-    }
+import Searchbar from "@/components/Searchbar";
 
-    fetch('http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=5')
-        .then(response => response.json())
-        .then(data => console.log(data));
+export default {
+  name: 'App',
+  components: {
+    Searchbar
+  },
+  methods: {
+    fetchData() {
+      fetch('http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=5')
+          .then(response => response.json())
+          .then(data => console.log(data));
+    }
+  }
+}
 
 
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
