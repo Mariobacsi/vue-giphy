ea8eaed163b53048d642a5fdf4ff244a16567f0f
Mario Seidl <mario.s-@live.de>
2020-09-21 13:22:44 +0200

    Searchbar exkludiert und einfach in App ein Input gemacht + GiphList

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,50 @@
 <template>
-    <div id="app">
+  <div id="app">
 
-        <searchbar :search="data"></searchbar>
-        <button v-on:click="fetchData">Fetch</button>
-        <!--<br>Number of Giphs:<input v-model="number_of_giphs" placeholder="Number of Giphs">-->
-        <!--<p>Message is: {{ search }}</p>-->
+    <b-form-input v-model="search" placeholder="Search"/>
+    <button v-on:click="fetchData">Search</button>
+    <giph-list data=""></giph-list>
 
-    </div>
+  </div>
 </template>
 
 <script>
 
+import GiphList from "@/components/GiphList";
 
-    import Searchbar from "@/components/Searchbar";
-
-    export default {
-        name: 'App',
-        data() {
-            return {
-                number_of_giphs: "100",
-                data:''
-            }
-        },
-        components: {
-            Searchbar
-        },
-        methods: {
-            fetchData() {
-                var searchword = document.getElementById("tmp").innerText;
-                console.log(searchword);
-
-                fetch('http://api.giphy.com/v1/gifs/search?q=' + searchword +'&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=10')
-                    .then(response => response.json())
-                    .then(data => console.log(data));
-            }
-        }
+export default {
+  name: 'App',
+  data() {
+    return {
+      number_of_giphs: "100",
+      data: '',
+      search: ''
     }
+  },
+  components: {
+    GiphList
+  },
+  methods: {
+    fetchData() {
+      var searchword = document.getElementById("tmp").innerText;
+      console.log(searchword);
+
+      fetch('http://api.giphy.com/v1/gifs/search?q=' + this.search + '&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=10')
+          .then(response => response.json())
+          .then(data => console.log(data));
+    }
+  }
+}
 
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
---
+++ src/components/GiphList.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <b-card v-for="giph in data" :key="giph.id"
+            :img-src="giph.images.original.url"
+            :title="giph.title"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GiphList",
+  props: ['data']
+}
+</script>
+
+<style scoped>
+
+</style>
