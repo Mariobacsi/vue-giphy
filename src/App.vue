36ca5f5611d40739f2dc4897a645727b3026ab9f
Christopher Scherling <christopher.scherling@gmail.com>
2020-09-21 13:07:25 +0200

    Searchbar geht

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,51 @@
 <template>
-  <div id="app">
+    <div id="app">
 
-    <searchbar></searchbar>
-    <button v-on:click="fetchData">Fetch</button>
-    <!--<br>Number of Giphs:<input v-model="number_of_giphs" placeholder="Number of Giphs">-->
-    <!--<p>Message is: {{ search }}</p>-->
+        <searchbar :search="data"></searchbar>
+        <button v-on:click="fetchData">Fetch</button>
+        <!--<br>Number of Giphs:<input v-model="number_of_giphs" placeholder="Number of Giphs">-->
+        <!--<p>Message is: {{ search }}</p>-->
 
-  </div>
+    </div>
 </template>
 
 <script>
 
 
-import Searchbar from "@/components/Searchbar";
+    import Searchbar from "@/components/Searchbar";
 
-export default {
-  name: 'App',
-    data() {
-        return {
-            number_of_giphs: "100"
+    export default {
+        name: 'App',
+        data() {
+            return {
+                number_of_giphs: "100",
+                data:''
+            }
+        },
+        components: {
+            Searchbar
+        },
+        methods: {
+            fetchData() {
+                var searchword = document.getElementById("tmp").innerText;
+                console.log(searchword);
+
+                fetch('http://api.giphy.com/v1/gifs/search?q=' + searchword +'&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=10')
+                    .then(response => response.json())
+                    .then(data => console.log(data));
+            }
         }
-    },
-  components: {
-    Searchbar
-  },
-  methods: {
-    fetchData() {
-
-
-      fetch('http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=5')
-          .then(response => response.json())
-          .then(data => console.log(data));
     }
-  }
-}
 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,19 +1,15 @@
 <template>
-    <div class="input-group md-form form-sm form-1 pl-0">
-        <div class="input-group-prepend">
-      <span class="input-group-text cyan lighten-2" id="basic-text1">
-        <i class="fas fa-search text-white" aria-hidden="true"></i>
-      </span>
-        </div>
-        <input class="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search" v-model="search_one">
+    <div>
+        <b-form-input v-model="search_one" placeholder="Enter your name"></b-form-input>
         <button v-on:click="search_two=search_one">Search</button>
-        <p>Message is: {{ search_two }}</p>
+        <p class="type='hidden'" id="tmp">{{ search_two }}</p>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Searchbar", data() {
+        name: "Searchbar",
+        data() {
             return {search_one: "", search_two: ""}
         }
     }
