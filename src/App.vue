cc7d276ff49762051e815b8912a9e3671e571dba
Mario Seidl <mario.s-@live.de>
2020-09-21 11:48:00 +0200

    Bootstrap Plugin  hinzugefügt

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,12 @@
 <template>
   <div id="app">
-
-    <searchbar/>
-
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-
-
-import Searchbar from "@/components/Searchbar";
-
-export default {
-  name: 'App',
-  components: {
-    Searchbar
-  },
-  methods: {
-    fetchData() {
-      fetch('http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=5')
-          .then(response => response.json())
-          .then(data => console.log(data));
-    }
-  }
-}
-
-
-</script>
 
 <style>
 #app {
@@ -35,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -1,11 +1,11 @@
 <template>
-  <div>
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">@</span>
-      </div>
-      <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+  <div class="input-group md-form form-sm form-1 pl-0">
+    <div class="input-group-prepend">
+      <span class="input-group-text cyan lighten-2" id="basic-text1">
+        <i class="fas fa-search text-white" aria-hidden="true"></i>
+      </span>
     </div>
+    <input class="form-control my-0 py-1" type="text" placeholder="Search" aria-label="Search">
   </div>
 </template>
 
