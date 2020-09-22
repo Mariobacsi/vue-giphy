fcbdbe2c21c70d9b5a42a356239dc5a49a286c00
Christopher Scherling <christopher.scherling@gmail.com>
2020-09-22 15:36:09 +0200

    Searchbar geht final

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,51 @@
 <template>
-  <div id="app">
+    <div id="app">
 
-    <b-form-input v-model="search" placeholder="Search"/>
-    <b-button v-on:click="fetchData">Search</b-button>
-    <giph-list data=""></giph-list>
+        <b-form-input v-model="search" placeholder="Enter your name"></b-form-input>
+        <b-button v-on:click="fetchData">los</b-button>
+        <giph-list data=""></giph-list>
 
-  </div>
+    </div>
 </template>
 
 <script>
 
-import GiphList from "@/components/GiphList";
+    import GiphList from "@/components/GiphList";
 
-export default {
-  name: 'App',
-    components: {
-    GiphList
-  },
-  data() {
-    return {
-      number_of_giphs: "100",
-      data: '',
-      search: ''
-    }
-  },
-  methods: {
-    fetchData() {
-      var searchword = document.getElementById("tmp").innerText;
-      console.log(searchword);
+    export default {
+        name: 'App',
+        components: {
+            GiphList
+        },
+        data() {
+            return {
+                number_of_giphs: "100",
+                data: '',
+                search: ''
+            }
+        },
+        methods: {
+            fetchData() {
+                // var searchword = document.getElementById("tmp").innerText;
+                console.log(this.search);
+                var limit = 10;
 
-      fetch('http://api.giphy.com/v1/gifs/search?q=' + this.search + '&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=10')
-          .then(response => response.json())
-          .then(data => console.log(data));
+                fetch('http://api.giphy.com/v1/gifs/search?q=' + this.search + '&api_key=ZCzC619AGEr8Re7Xz67lmb49QEg2aX9O&limit=' + limit)
+                    .then(response => response.json())
+                    .then(data => console.log(data));
+            }
+        }
     }
-  }
-}
 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
