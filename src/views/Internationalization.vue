87f2e0bf0f7ff9d148428508f85a38b9b7154912
Mario Seidl <mario.s-@live.de>
2020-10-06 16:03:44 +0200

    Datepicker

--- src/views/Internationalization.vue
+++ src/views/Internationalization.vue
@@ -1,15 +1,9 @@
 <template>
   <div>
     <h1>Title</h1>
-    <b-button v-on:click="show_date = !show_date">Show Date</b-button>
-    <div v-if="show_date">
-      <b-row>
-        <b-form-select class="col-md-1" v-model="date_day" :options="date_day_limit"/>
-        <b-form-select class="col-md-2" v-model="date_month" :options="$t('internationalization.date.month_limit')"/>
-        <b-form-input class="col-md-1" v-model="date_year"/>
-      </b-row>
-      <h2>{{ date_day }}.{{ date_month }}.{{ date_year }}</h2>
-    </div>
+    <b-form-datepicker v-model="date"
+                       :locale="$i18n.locale"/>
+    <h2>{{ date }}</h2>
     <!--<p>{{ money.message }}: {{ money.icon }}</p>-->
   </div>
 </template>
@@ -20,9 +14,7 @@ export default {
   data() {
     return {
       show_date: false,
-      date_day: 0,
-      date_month: "",
-      date_year: 0
+      date: ""
     }
   }
 }
