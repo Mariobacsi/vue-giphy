1001fe23893819162666181be504a6d73b480be2
Mario Seidl <mario.s-@live.de>
2020-10-06 16:04:11 +0200

    fixed links

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,55 @@
 <!--Team: Seidl, Kraljevic, Scherling-->
 <template>
-    <div id="app">
+  <div id="app">
 
-        <!--Links zu den verschiedenen Views(Routers)-->
-        <b-row>
-            <b-row class="col-md">
-                <router-link to="/giphy">
-                    <b-button>Giphy</b-button>
-                </router-link>
-                <router-link to="/international">
-                    <b-button>International</b-button>
-                </router-link>
-            </b-row>
-            <!--Sprachselector-->
-            <div class="locale-changer ">
-                <select v-model="$i18n.locale" class="col-md-1">
-                    <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
-                </select>
-            </div>
-        </b-row>
+    <!--Links zu den verschiedenen Views(Routers)-->
+    <b-row>
+      <b-row class="col-md">
+        <router-link to="/giphy">
+          <b-button>Giphy</b-button>
+        </router-link>
+        <router-link to="/internationalization">
+          <b-button>International</b-button>
+        </router-link>
+      </b-row>
+      <!--Sprachselector-->
+      <div class="locale-changer ">
+        <select v-model="$i18n.locale" class="col-md-1">
+          <option v-for="(lang, i) in langs" :key="`Lang${i}`" :value="lang">{{ lang }}</option>
+        </select>
+      </div>
+    </b-row>
 
-        <router-view></router-view>
-        <p>{{ $t('internationalization.text') }}</p>
-        <b-button>{{$t('internationalization.button-yes')}}</b-button>
+    <p>{{ $t('internationalization.text') }}</p>
+    <b-button class="mb-5">{{ $t('internationalization.button-yes') }}</b-button>
 
-    </div>
+    <router-view></router-view>
+
+
+  </div>
 </template>
 
 <script>
 
-    export default {
-        name: 'App',
-        data() {
-            return {langs: ['en', 'de', 'ar']}
-        }
+export default {
+  name: 'App',
+  data() {
+    return {
+      langs: ['en', 'de', 'ar']
     }
+  }
+}
 
 </script>
 
 <style>
-    #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin: 60px auto auto;
-        width: 90%;
-    }
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin: 60px auto auto;
+  width: 90%;
+}
 </style>
