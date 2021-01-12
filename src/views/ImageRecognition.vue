45fcd94678f3daba8748f7b25118885cae7b7922
Mario Seidl <mario.s-@live.de>
2021-01-12 15:43:54 +0100

    init

---
+++ src/views/ImageRecognition.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="">
+
+  </div>
+</template>
+
+<script>
+import Axios from "axios";
+
+export default {
+  name: "ImageRecognition",
+  data(){
+    return{
+      ulr: "",
+      key: "acc_2db7600b6fe4ac0"
+    }
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
