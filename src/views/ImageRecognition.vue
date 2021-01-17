b3370967cccf89c781a543dc2afed51c00a2aaa5
Mario Seidl <mario.s-@live.de>
2021-01-17 18:58:31 +0100

    Am arbeiten

--- src/views/ImageRecognition.vue
+++ src/views/ImageRecognition.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="">
-
+    <button class="btn btn-secondary" @click="getData">get Data</button>
   </div>
 </template>
 
@@ -11,11 +11,20 @@ export default {
   name: "ImageRecognition",
   data(){
     return{
-      ulr: "",
-      key: "acc_2db7600b6fe4ac0"
+      imageUrl: 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg',
+      apiKey: "acc_2db7600b6fe4ac0",
+      apiSecret: "40c7c466795f517d0abfdcae3be834f6",
+      streaming: false
     }
   },
   methods: {
+    getData(){
+      Axios.get('https://api.imagga.com/v2/tags?image_url=' + encodeURIComponent(this.imageUrl), {
+        username: this.apiKey,
+        password: this.apiSecret
+      }).then(response => console.log(response))
+      .catch(error => console.error(error))
+    }
   }
 }
 </script>
