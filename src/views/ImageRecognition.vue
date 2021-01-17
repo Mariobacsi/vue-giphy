e45e6f6ef8da66a4478bb880c4a4bd82ff8c4082
Mario Seidl <mario.s-@live.de>
2021-01-17 20:17:04 +0100

    input

--- src/views/ImageRecognition.vue
+++ src/views/ImageRecognition.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="col row">
     <div class="col-8">
-      <button class="btn btn-secondary" @click="getData">get Data</button>
+      <div class="input-group">
+        <input class="form-control" type="url" v-model="imageUrl" placeholder="Image URL">
+        <button class="btn btn-secondary" type="button" @click="getData">get Data</button>
+      </div>
     </div>
     <tag-list class="col-4" :data="this.tags"></tag-list>
   </div>
@@ -17,7 +20,7 @@ export default {
   data() {
     return {
       tags: [],
-      imageUrl: 'https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg',
+      imageUrl: '', //https://docs.imagga.com/static/images/docs/sample/japan-605234_1280.jpg
       apiKey: "acc_2db7600b6fe4ac0",
       apiSecret: "40c7c466795f517d0abfdcae3be834f6",
       streaming: false
